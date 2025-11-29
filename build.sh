3ebf49ea33868329942653c2cb39c03fd2590cba
daniil <hynslei@ya.ru>
2025-11-29 21:31:18 +0300

    Я устроюсь на ПВЗ Ozon

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [ -f /opt/synapse/build.lock ]; then
+  exit 0
+fi
+
 touch /opt/synapse/build.lock
-docker compose up -d --build --remova-orphans
+
+docker compose up -d --build --remove-orphans
+
 rm /opt/synapse/build.lock
